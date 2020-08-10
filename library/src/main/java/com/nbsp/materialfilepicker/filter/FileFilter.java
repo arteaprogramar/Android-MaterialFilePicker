618de6d2136b34be4de6f72da97db03a8efeb391
arteaprogramar <arte.aprogramar@gmail.com>
2020-08-10 12:26:45 -0500

    Android 10, Material Components & Night Mode Support

--- library/src/main/java/com/nbsp/materialfilepicker/filter/FileFilter.java
+++
@@ -1,8 +0,0 @@
-package com.nbsp.materialfilepicker.filter;
-
-import java.io.File;
-import java.io.Serializable;
-
-public interface FileFilter extends Serializable {
-    boolean accept(File pathname);
-}
